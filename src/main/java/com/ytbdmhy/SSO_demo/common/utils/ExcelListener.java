0bf822a22d71a4e08ab119d1275bc6c0a61a43aa
ytbdmhy <568870206@qq.com>
2019-06-12 10:29:41 +0800

    1

---
+++ src/main/java/com/ytbdmhy/SSO_demo/common/utils/ExcelListener.java
@@ -0,0 +1,40 @@
+package com.ytbdmhy.SSO_demo.common.utils;
+
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Copyright: weface
+ * @Description:
+ * @author: miaohaoyun
+ * @since:
+ * @history: created in 09:17 2019-06-12 created by miaohaoyun
+ */
+public class ExcelListener extends AnalysisEventListener {
+
+    private List<Object> datas = new ArrayList<>();
+
+    @Override
+    public void invoke(Object o, AnalysisContext analysisContext) {
+        datas.add(o);
+        doSomething(o);
+    }
+
+    private void doSomething(Object o) {}
+
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext analysisContext) {
+        datas.clear();
+    }
+
+    public List<Object> getDatas() {
+        return datas;
+    }
+
+    public void setDatas(List<Object> datas) {
+        this.datas = datas;
+    }
+}
--- src/main/java/com/ytbdmhy/SSO_demo/common/utils/POIUtil.java
+++ src/main/java/com/ytbdmhy/SSO_demo/common/utils/POIUtil.java
@@ -1,12 +1,17 @@
 package com.ytbdmhy.SSO_demo.common.utils;
 
+import com.alibaba.excel.EasyExcelFactory;
+import com.alibaba.excel.ExcelReader;
+import com.alibaba.excel.support.ExcelTypeEnum;
+import com.monitorjbl.xlsx.StreamingReader;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,12 +21,102 @@ import java.util.List;
  * @author: miaohaoyun
  * @since:
  * @history: created in 10:02 2019-06-11 created by miaohaoyun
+ * @Remarks: poi 3.17; poi-ooxml 3.17; poi-scratchpad 3.17
  */
 @Slf4j
 public class POIUtil {
 
+    public static void readExcel2(String filePath) {
+        readExcel2(new File(filePath));
+    }
+
+    public static void readExcel2(File file) {
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            log.error("add message error", e);
+        }
+        ExcelListener excelListener = new ExcelListener();
+        EasyExcelFactory.readBySax(inputStream, new com.alibaba.excel.metadata.Sheet(1,1), excelListener);
+        List result = excelListener.getDatas();
+        System.out.println("over");
+    }
+
+    public static List readExcel(String filePath) {
+        File file = new File(filePath);
+        return readExcel(file);
+    }
+
     public static List readExcel(File file) {
-        List result = new LinkedList();
+        if (file == null)
+            throw new NullPointerException("excel不存在");
+        String fileName = file.getName();
+        if (!fileName.toLowerCase().endsWith(".xls") && !fileName.toLowerCase().endsWith(".xlsx"))
+            throw new NullPointerException("读取的文件不是excel");
+        List<String[]> result = new LinkedList<>();
+
+        Workbook workbook = null;
+        try {
+            // 获取文件的IO流
+            InputStream inputStream = new FileInputStream(file);
+            if (fileName.toLowerCase().endsWith(".xls")) {
+                workbook = WorkbookFactory.create(inputStream);
+            } else if (fileName.toLowerCase().endsWith(".xlsx")) {
+                workbook = StreamingReader.builder()
+                        .rowCacheSize(100)
+                        .bufferSize(4096)
+                        .open(inputStream);
+            }
+        } catch (FileNotFoundException e) {
+            log.error("add message error", e);
+        } catch (InvalidFormatException e) {
+            log.error("add message error", e);
+        } catch (IOException e) {
+            log.error("add message error", e);
+        }
+        if (workbook != null) {
+            for (int sheetNum = 0; sheetNum < workbook.getNumberOfSheets(); sheetNum++) {
+                if (fileName.toLowerCase().endsWith(".xls")) {
+                    Sheet sheet = workbook.getSheetAt(sheetNum);
+//                    if (sheet == null)
+//                        continue;
+                    for (int rowNum = sheet.getFirstRowNum(); rowNum < sheet.getLastRowNum(); rowNum++) {
+                        String[] rowValue = new String[sheet.getLastRowNum() - sheet.getFirstRowNum() + 1];
+                        Row row = sheet.getRow(rowNum);
+//                        if (row == null)
+//                            continue;
+                        for (int cellNum = row.getFirstCellNum(); cellNum < row.getLastCellNum(); cellNum++) {
+                            Cell cell = row.getCell(cellNum);
+//                            if (cell == null)
+//                                continue;
+                            rowValue[cellNum] = cell.getStringCellValue();
+                        }
+                        result.add(rowValue);
+                    }
+                } else {
+                    Sheet sheet = workbook.getSheetAt(sheetNum);
+                    for (Row row : sheet) {
+                        String[] rowValue = new String[row.getPhysicalNumberOfCells()];
+                        int i = 0;
+                        for (Cell cell : row) {
+                            rowValue[i] = cell.getStringCellValue();
+                            i++;
+                        }
+                        result.add(rowValue);
+                    }
+                }
+            }
+        }
+        try {
+            workbook.close();
+        } catch (IOException e) {
+            log.error("add message error", e);
+        }
+
+        System.out.println("------------------------------------");
+        System.out.println(Arrays.toString(result.get(0)));
+        System.out.println(Arrays.toString(result.get(result.size()-1)));
         return result;
     }
 
@@ -59,10 +154,10 @@ public class POIUtil {
         cellStyle.setWrapText(false);
         int i = 0;
         if (firstRow.length > 0) {
+            Row row = sheet.createRow(0);
+            // 设置行高
+            row.setHeight((short) (20 * 15));
             for (String colData : firstRow) {
-                Row row = sheet.createRow(0);
-                // 设置行高
-//            row.setHeight((short) (20 * 15));
                 // 创建对应的单元格
                 Cell cell = row.createCell(i);
                 // 设置单元格的数据类型为文本
@@ -95,16 +190,23 @@ public class POIUtil {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("start");
         long startTime = System.currentTimeMillis();
 
-        String exportPath = "D:/WorkFile/test.xlsx";
-        List<Object[]> dataList = new LinkedList<>();
-        for (int i = 0;i < 1000000;i++) {
-            dataList.add(new String[]{"data-"+i+"-1", "data-"+i+"-2", "data-"+i+"-3"});
-        }
-        exportExcel(exportPath, new String[]{"id","name","age"}, dataList);
+        // readExcel2
+//        readExcel2("D:\\WorkFile\\0606-副本\\大名已制卡名单\\合并excel\\allExcel.xlsx");
+
+        // readExcel
+        List excelData = readExcel("D:\\WorkFile\\0606-副本\\大名已制卡名单\\合并excel\\allExcellogDate.xlsx");
+
+        // exportExcel
+//        String exportPath = "D:/WorkFile/test.xlsx";
+//        List<Object[]> dataList = new LinkedList<>();
+//        for (int i = 0;i < 1000000;i++) {
+//            dataList.add(new String[]{"data-"+i+"-1", "data-"+i+"-2", "data-"+i+"-3"});
+//        }
+//        exportExcel(exportPath, new String[]{"id","name","age"}, dataList);
 
         System.out.println("over,用时:" + (System.currentTimeMillis() - startTime) + "毫秒");
     }
